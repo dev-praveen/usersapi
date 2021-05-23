043798103823878ae19aa8cdc1c7f3698b09f8dd
Praveen Sana <spraveen0244@gmail.com>
2021-05-23 16:06:06 +0530

    refactored code

--- src/main/java/com/example/demo/CustomTodosUserResponse.java
+++ src/main/java/com/example/demo/CustomTodosUserResponse.java
@@ -1,7 +1,6 @@
 package com.example.demo;
 
 import lombok.Data;
-
 import java.util.List;
 
 @Data
--- src/main/java/com/example/demo/UserAPI.java
+++ src/main/java/com/example/demo/UserAPI.java
@@ -1,10 +1,12 @@
 package com.example.demo;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.lang.NonNull;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
+
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
@@ -17,25 +19,21 @@ public class UserAPI {
 
     private RestTemplate restTemplate = new RestTemplate();
 
-    private CustomUserResponseDTO apply(UserDTO user) {
-
-        CustomUserResponseDTO userResponseDTO = new CustomUserResponseDTO();
-
-        userResponseDTO.setId(user.getId());
-        userResponseDTO.setUserName(user.getUsername());
-        userResponseDTO.setCompanyName(user.getCompany().getName());
-        userResponseDTO.setGeo(user.getAddress().getGeo());
-
-        return userResponseDTO;
-    }
-
     @GetMapping("/users")
     public ResponseEntity<CustomUserResponse> getUsersInfo() {
 
         CustomUserResponse userResponse = new CustomUserResponse();
+        List<CustomUserResponseDTO> customUserResponseDTOS = new ArrayList<>();
         ResponseEntity<UserDTO[]> response = restTemplate.getForEntity("https://jsonplaceholder.typicode.com/users", UserDTO[].class);
-        final UserDTO[] userDTOS = response.getBody();
-        final List<CustomUserResponseDTO> customUserResponseDTOS = Arrays.stream(userDTOS).map(userDTO -> apply(userDTO)).collect(Collectors.toList());
+
+        boolean isError = response.getStatusCode().isError();
+
+        if (!isError) {
+            final UserDTO[] userDTOS = response.getBody();
+            if (null != userDTOS && userDTOS.length > 0) {
+                customUserResponseDTOS = Arrays.stream(userDTOS).map(userDTO -> apply(userDTO)).collect(Collectors.toList());
+            }
+        }
         userResponse.setCustomUserResponseDTOSList(customUserResponseDTOS);
 
         return ResponseEntity.ok(userResponse);
@@ -50,38 +48,54 @@ public class UserAPI {
         UserDTO[] userDTOS = restTemplate.getForObject("https://jsonplaceholder.typicode.com/users", UserDTO[].class);
         UsersTodoDTO[] usersTodoDTOS = restTemplate.getForObject("https://jsonplaceholder.typicode.com/todos", UsersTodoDTO[].class);
 
-        for (UserDTO userDTO : userDTOS) {
+        if (null != userDTOS && userDTOS.length > 0) {
 
-            CustomUsersTodoDTO usersTodoDTO = new CustomUsersTodoDTO();
-            usersTodoDTO.setUserName(userDTO.getUsername());
-            usersTodoDTO.setCompanyName(userDTO.getCompany().getName());
+            for (UserDTO userDTO : userDTOS) {
 
-            Integer completedTasks = 0;
-            Integer notCompletedTasks = 0;
+                Integer completedTasks = 0;
+                Integer notCompletedTasks = 0;
 
-            for (UsersTodoDTO todoDTO : usersTodoDTOS) {
+                CustomUsersTodoDTO usersTodoDTO = new CustomUsersTodoDTO();
+                usersTodoDTO.setUserName(userDTO.getUsername());
+                usersTodoDTO.setCompanyName(userDTO.getCompany().getName());
 
-                if (userDTO.getId() == todoDTO.getUserId()) {
+                if (null != usersTodoDTOS && usersTodoDTOS.length > 0) {
 
-                    Boolean isCompleted = todoDTO.getCompleted();
-                    if (Boolean.TRUE.equals(isCompleted)) {
-                        completedTasks++;
+                    for (UsersTodoDTO todoDTO : usersTodoDTOS) {
+
+                        if (userDTO.getId() == todoDTO.getUserId()) {
+
+                            Boolean isCompleted = todoDTO.getCompleted();
+                            if (Boolean.TRUE.equals(isCompleted)) {
+                                completedTasks++;
+                            } else {
+                                notCompletedTasks++;
+                            }
+                        }
                     }
-                    if (Boolean.FALSE.equals(isCompleted)) {
-                        notCompletedTasks++;
-                    }
-                    continue;
                 }
+
+                usersTodoDTO.setCompletedTasks(completedTasks);
+                usersTodoDTO.setNotCompletedTasks(notCompletedTasks);
+
+                usersTodoDTOList.add(usersTodoDTO);
             }
-
-            usersTodoDTO.setCompletedTasks(completedTasks);
-            usersTodoDTO.setNotCompletedTasks(notCompletedTasks);
-
-            usersTodoDTOList.add(usersTodoDTO);
         }
 
         Collections.sort(usersTodoDTOList);
         customTodosUserResponse.setCustomUsersTodoDTOS(usersTodoDTOList);
         return ResponseEntity.ok(customTodosUserResponse);
+    }
+
+    private CustomUserResponseDTO apply(@NonNull UserDTO user) {
+
+        CustomUserResponseDTO userResponseDTO = new CustomUserResponseDTO();
+
+        userResponseDTO.setId(user.getId());
+        userResponseDTO.setUserName(user.getUsername());
+        userResponseDTO.setCompanyName(user.getCompany().getName());
+        userResponseDTO.setGeo(user.getAddress().getGeo());
+
+        return userResponseDTO;
     }
 }
